c0aefddb09117ff1872727efffc33232742ef577
adiaz129 <andrewdiaz@gmail.com>
2026-03-12 22:54:23 -0700

    Added near density and near pressure calculations for water cohesion look. - Very difficult to dial the settings correctly so that both th surface tension and near pressure have a good effect on the simulation - The efficiency has decreased so I must find a way to improve it again - Found that smaller bounds and a smaller scale looks less slow motion but handles less particles - Having near stiffness and stiffness is helpful due to near pressure being unable to be negative so I could control the repelling force separately

--- Assets/Scripts/SPHFluid2D.cs
+++ Assets/Scripts/SPHFluid2D.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -5,7 +6,9 @@ public class SPHFluid2D
 {
     private Vector2[] _predPos;
     private float[] _density;
+    private float[] _nearDensity;
     private float[] _pressure;
+    private float[] _nearPressure;
     private Vector2[] _velocity;
     private int[] _neighbors;
     private float[] _normals;
@@ -16,31 +19,35 @@ public class SPHFluid2D
     public float smoothingRadius = 0.35f;
     public float restDensity = 22;
     public float stiffness = 22;
+    public float nearStiffness = 22;
     public float minDensity = 1e-4f;
     public float viscosityStrength = 0.5f;
     public float surfaceThreshold = 0.2f;
     public float surfTensMultiplier = 3f;
 
 
-    public SPHFluid2D(Vector2[] predictedPosition, float[] density, float[] pressure, Vector2[] velocity, int[] neighbors, float[] normals, SpatialHash2D grid)
+    public SPHFluid2D(Vector2[] predictedPosition, float[] density, float[] nearDensity, float[] pressure, float[] nearPressure, Vector2[] velocity, int[] neighbors, float[] normals, SpatialHash2D grid)
     {
-        Bind(predictedPosition, density, pressure, velocity, neighbors, normals, grid);
+        Bind(predictedPosition, density, nearDensity, pressure, nearPressure, velocity, neighbors, normals, grid);
     }
 
-    public void Bind(Vector2[] predictedPosition, float[] density, float[] pressure, Vector2[] velocity, int[] neighbors, float[] normals, SpatialHash2D grid)
+    public void Bind(Vector2[] predictedPosition, float[] density, float[] nearDensity, float[] pressure, float[] nearPressure, Vector2[] velocity, int[] neighbors, float[] normals, SpatialHash2D grid)
     {
         _predPos = predictedPosition;
         _density = density;
+        _nearDensity = nearDensity;
         _pressure = pressure;
+        _nearPressure = nearPressure;
         _velocity = velocity;
         _neighbors = neighbors;
         _normals = normals;
         _grid = grid;
     }
 
-    public float DensityAt(Vector2 samplePoint)
+    public (float resultDensity, float resultNearDensity) DensityAt(Vector2 samplePoint)
     {
         float resultDensity = 0;
+        float resultNearDensity = 0;
         var baseCell = _grid.CellCoord(samplePoint);
         float r2 = smoothingRadius * smoothingRadius;
 
@@ -59,19 +66,20 @@ public class SPHFluid2D
                 int k = _grid.spatialLookup[j].particleIndex;
                 float sqrDist = (_predPos[k] - samplePoint).sqrMagnitude;
                 if (sqrDist >= r2) continue;
-                float dist = Mathf.Sqrt(sqrDist);
-                float influence = SPHMath2D.Poly6_2D(smoothingRadius, dist);
+                var (influence, nearInfluence) = SPHMath2D.Poly6_2D(sqrDist);
                 resultDensity += mass * influence;
+                resultNearDensity += mass * nearInfluence;
             }
         }
-        return resultDensity;
+        return (resultDensity, resultNearDensity);
     }
 
-    public float ConvertDensityToPressure(float dens)
+    public (float pressure, float nearPressure) ConvertDensityToPressure(float dens, float nearDens)
     {
         float densityError = dens - restDensity;
-        float pressure = densityError * stiffness; //change this when adding near pressure (as in remove max)
-        return pressure;
+        float pressure = densityError * stiffness;
+        float nearPressure = nearDens * nearStiffness;
+        return (pressure, nearPressure);
     }
 
     public Vector2 PressureAndViscosityAt(int pointIndex)
@@ -82,6 +90,8 @@ public class SPHFluid2D
         float curvature = 0f;
         var baseCell = _grid.CellCoord(_predPos[pointIndex]);
         float r2 = smoothingRadius * smoothingRadius;
+        float currDensity = Mathf.Max(_density[pointIndex], minDensity);
+        float currNearDensity = Mathf.Max(_nearDensity[pointIndex], minDensity);
 
         for (int i = 0; i < neighborOffsets.Length; i++)
         {
@@ -104,34 +114,38 @@ public class SPHFluid2D
                 
                 if (sqrDist >= r2) continue;
                 _neighbors[pointIndex]++;
-                float dist = Mathf.Sqrt(sqrDist);
-                float slope = SPHMath2D.SpikyGrad_2D(smoothingRadius, dist);
-                Vector2 dir = (dist < 1e-6f) ? SPHMath2D.PseudoRandomUnitVector(pointIndex, k) : offset / dist;
+                Vector2 slope = SPHMath2D.Poly6Grad_2D(sqrDist, offset);
+                Vector2 nearSlope = SPHMath2D.NearPoly6Grad_2D(sqrDist, offset);
 
-                float currDensity = Mathf.Max(_density[pointIndex], minDensity);
                 float kDensity = Mathf.Max(_density[k], minDensity);
+                float kNearDensity = Mathf.Max(_nearDensity[k], minDensity);
 
                 float pressureTerm = (_pressure[pointIndex] + _pressure[k]) / (2 * currDensity * kDensity);
-                totalForce += pressureTerm * mass * slope * dir;
+                totalForce += pressureTerm * mass * slope;
 
-                float laplacian = SPHMath2D.ViscosityLaplacian_2D(smoothingRadius, dist);
+                float nearPressureTerm = (_nearPressure[pointIndex] + _nearPressure[k]) / (2 * currNearDensity * kNearDensity);
+                totalForce += nearPressureTerm * mass * nearSlope;
+
+                float laplacian = SPHMath2D.Poly6Laplacian_2D(sqrDist);
                 Vector2 velocityDiff = _velocity[k] - _velocity[pointIndex];
 
                 massOverDensity = mass / kDensity;
-                normal += massOverDensity * SPHMath2D.Poly6Grad_2D(smoothingRadius, dist, offset);
-                curvature += massOverDensity * SPHMath2D.Poly6Laplacian_2D(smoothingRadius, dist);
+                normal += massOverDensity * slope;
+                curvature += massOverDensity * laplacian;
 
-                totalForce += viscosityStrength * mass * velocityDiff / kDensity * laplacian;
+                float vLaplacian = SPHMath2D.ViscosityLaplacian_2D(sqrDist);
+                totalForce += viscosityStrength * mass * velocityDiff / kDensity * vLaplacian;
             }
         }
-        _normals[pointIndex] = normal.magnitude;
+        _normals[pointIndex] = normal.sqrMagnitude;
 
         Vector2 surfaceForce = Vector2.zero;
-        if (_normals[pointIndex] > surfaceThreshold)
+
+        if (_normals[pointIndex] > Mathf.Max(surfaceThreshold, minDensity))
         {
-            Vector2 nHat = normal / _normals[pointIndex];
+            Vector2 nHat = normal / Mathf.Sqrt(_normals[pointIndex]);
             surfaceForce = -surfTensMultiplier * curvature * nHat;
-            surfaceForce /= Mathf.Max(_density[pointIndex], minDensity);
+            surfaceForce /= currDensity;
         }
         return totalForce + surfaceForce;
     }
